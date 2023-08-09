92f1ee1f7c35a696a495317fd87465ab61d9f311
hayatkhan67 <hayatkhan626225@gmail.com>
2023-08-09 11:15:35 +0500

    some fixes

--- lib/Login_page/sign_up.dart
+++ lib/Login_page/sign_up.dart
@@ -1,7 +1,9 @@
 import 'package:bano_qabil_project/widget/customText.dart';
 import 'package:bano_qabil_project/widget/custom_ElevatedButton.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
+import '../widget/custom_appbar.dart';
 import 'create_account.dart';
 import 'forget_paasword.dart';
 
@@ -10,56 +12,86 @@ class SignUp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+  final  screenmd=MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-          leading: IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: const Icon(Icons.arrow_back_ios)),
-      ),
-      body:  Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-           const Row(
-                children: [
-                  MyText(text: 'Join Facebook',),
-                ],
+      appBar: const CustomAppBar(),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Row(
+                      children: [
+                        MyText(
+                          text: 'Join Facebook',
+                        ),
+                      ],
+                    ),
+                   
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10.0),
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: SvgPicture.asset(
+                            'assets/svg/ddc-facebook-audience-insights-p-500.svg',
+                            fit: BoxFit.fill,height: screenmd.height *.3,width: double.infinity,
+                          )),
+                    ),
+                    const MyText(
+                      text:
+                          "Create an account to connect with friends,family and communities of people who share your interests",
+                      fWeight: FontWeight.w500,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    CustomElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const CreateAccount(),
+                            ));
+                      },
+                      color: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      sizeHeight: 45,
+                      child: const MyText(
+                        text: 'Get Started',
+                        color: Colors.white,
+                        fWeight: FontWeight.w500,
+                      ),
+                    ),
+                    // const Expanded(child: SizedBox()),
+                  ],
+                ),
               ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10.0),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: AspectRatio(
-                aspectRatio: 16/9,
-                child: Image.asset('assets/facebook/loginpage/62b03a20c7e36272df213ddc_Facebook Audience Insights-p-500.png',fit: BoxFit.cover,))),
-          ),
-            const MyText(text: "Create an account to connect with friends,family and communities of people who share your interests",fWeight: FontWeight.w500,),
-            const SizedBox(height: 10,),
-            CustomElevatedButton(
-              onPressed: (){
-                Navigator.push(context,MaterialPageRoute(builder: (context) => const CreateAccount(),));
-              },
-              color: Colors.blue,
-              shape:  RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20)
-              ),
-              sizeHeight: 45,
-              child:const MyText(text: 'Get Started',color: Colors.white,fWeight: FontWeight.w500,), 
             ),
-            const Expanded(child: SizedBox()),
-            Row(
+          ),
+          Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextButton(onPressed: (){
-                Navigator.push(context,MaterialPageRoute(builder: (context) => const ForgetPassword(),));
-              }, child: const MyText(text: 'Already have an account?',color: Color.fromARGB(255, 11, 132, 230),fWeight: FontWeight.bold,)),
+              TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const ForgetPassword(),
+                        ));
+                  },
+                  child: const MyText(
+                    text: 'Already have an account?',
+                    color: Color.fromARGB(255, 11, 132, 230),
+                    fWeight: FontWeight.bold,
+                  )),
             ],
           )
-          ],
-        ),
+        ],
       ),
     );
   }
--- lib/widget/custom_appbar.dart
+++ lib/widget/custom_appbar.dart
@@ -1,33 +1,44 @@
 import 'package:flutter/material.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSize {
-  const CustomAppBar({super.key, this.title, this.leadingWidth, this.centerTitle, this.elevation, this.actions, this.leading, this.barHeigth});
+  const CustomAppBar(
+      {super.key,
+      this.title,
+      this.leadingWidth,
+      this.centerTitle,
+      this.elevation,
+      this.actions,
+      this.leading,
+      this.barHeigth,});
 
-final Widget? title;
-final Widget? leading;
-final double? leadingWidth;
-final double? barHeigth;
-final bool? centerTitle;
-final double? elevation;
-final List<Widget>? actions;
+  final Widget? title;
+  final Widget? leading;
+  final double? leadingWidth;
+  final double? barHeigth;
+  final bool? centerTitle;
+  final double? elevation;
+  final List<Widget>? actions;
   @override
   Widget build(BuildContext context) {
-    return  AppBar(
-        title:title,
-        leading: leading,
-        centerTitle: centerTitle??false,
-        elevation: elevation,
-        leadingWidth: leadingWidth,
-        actions:actions,
-        toolbarHeight: barHeigth,
-
-
-      );
+    return AppBar(
+      title: title,
+      leading: leading !=null?
+          IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.arrow_back_ios)):leading,
+      centerTitle: centerTitle ?? false,
+      elevation: elevation,
+      leadingWidth: leadingWidth,
+      actions: actions,
+      toolbarHeight: barHeigth,
+    );
   }
-  
+
   @override
   Widget get child => throw UnimplementedError();
-  
+
   @override
-  Size get preferredSize => Size.fromHeight(barHeigth??56.0);
+  Size get preferredSize => Size.fromHeight(barHeigth ?? 56.0);
 }
