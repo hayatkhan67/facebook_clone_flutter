a416db87dce28444fe199617e4cd5299564628c2
hayatkhan67 <hayatkhan626225@gmail.com>
2023-08-10 12:57:59 +0500

    setting and bottom sheet fix

--- lib/Facebook/Menu/setting&privacy/my_bottom_sheet.dart
+++ lib/Facebook/Menu/setting&privacy/my_bottom_sheet.dart
@@ -1,3 +1,4 @@
+import 'package:bano_qabil_project/widget/custom_Divider.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -13,7 +14,6 @@ class MyBottomSheet extends StatefulWidget {
 class _MyBottomSheetState extends State<MyBottomSheet> {
   List accIcons = [
     Icons.account_circle_outlined,
-    // Icons.perm_device_info,
     FontAwesomeIcons.idBadge,
     Icons.shield_outlined,
     Icons.lock_clock_outlined,
@@ -31,178 +31,147 @@ class _MyBottomSheetState extends State<MyBottomSheet> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.grey.shade200,
-          leading: IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: const Icon(Icons.close)),
-          title: const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                FontAwesomeIcons.meta,
-                color: Colors.blue,
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              MyText(text: 'Meta'),
-            ],
-          ),
-        ),
-        body: Container(
-          height: MediaQuery.of(context).size.height * 1.9,
-          width: double.infinity,
-          decoration: BoxDecoration(
-              color: Colors.grey.shade200,
-              borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(20), topRight: Radius.circular(20))),
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: ListView(
-              // mainAxisAlignment: MainAxisAlignment.start,
-              // crossAxisAlignment: CrossAxisAlignment.start,
+      backgroundColor: Colors.grey[200],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
               children: [
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Center(
-                        child: MyText(
-                            text: 'Account Centre',
-                            size: 20.0,
-                            fWeight: FontWeight.bold)),
-                  ],
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Row(
-                  children: [
-                    Expanded(
-                      child: MyText(
-                        align: TextAlign.center,
-                        text:
-                            'Manage your connected experiences and account settings across Meta technologies such as Facebook,Instagram and Meta Horizon',
-                        size: 16.0,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
-                  ),
-                  height: 165,
-                  width: double.infinity,
-                  child: Column(
+                IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(Icons.close)),
+                const Align(
+                widthFactor: 4.6,
+                  child: Row(
                     children: [
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 8.0),
-                        child: ListTile(
-                          leading: Stack(
-                            clipBehavior: Clip.none,
-                            children: [
-                              CircleAvatar(
-                                backgroundColor: Colors.black,
-                                radius: 25,
-                                child: CircleAvatar(
-                                  radius: 22,
-                                  backgroundImage: AssetImage(
-                                      'assets/facebook/homepage/photo_2023-07-07_14-37-37.jpg'),
-                                ),
-                              ),
-                              Positioned(
-                                  top: 22,
-                                  right: 19,
-                                  child: CircleAvatar(
-                                    backgroundColor: Colors.black,
-                                    radius: 25,
-                                    child: CircleAvatar(
-                                      radius: 22,
-                                      backgroundImage: AssetImage(
-                                          'assets/facebook/homepage/photo_2023-07-07_14-37-37.jpg'),
-                                    ),
-                                  ))
-                            ],
-                          ),
-                          title: MyText(
-                            text: 'Profile',
-                            size: 17.0,
-                          ),
-                          subtitle: MyText(
-                            text: 'Hayat Khan ,hayatinsta',
-                            size: 17.0,
-                          ),
-                          trailing: Wrap(
-                            children: [
-                              MyText(
-                                text: '2',
-                                size: 17.0,
-                              ),
-                              Icon(
-                                Icons.arrow_forward_ios_rounded,
-                                size: 20.0,
-                              )
-                            ],
-                          ),
-                        ),
+                      Icon(
+                        FontAwesomeIcons.meta,
+                        color: Colors.blue,
+                        size: 15,
                       ),
-                      const SizedBox(
-                        height: 20,
+                      SizedBox(
+                        width: 10,
                       ),
-                      Divider(
-                        thickness: 2,
-                        color: Colors.grey.shade200,
-                      ),
-                      const Expanded(
-                        child: ListTile(
-                          leading: Icon(Icons.connect_without_contact_rounded),
-                          title: MyText(
-                            text: 'connected experiences',
-                            size: 20.0,
-                            fWeight: FontWeight.w400,
-                          ),
-                          trailing: Icon(
-                            Icons.arrow_forward_ios_sharp,
-                            size: 17.0,
-                          ),
-                        ),
+                      MyText(
+                        text: 'Meta',
+                        size: 17,
                       )
                     ],
                   ),
                 ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 20),
-                  child: MyText(
-                    text: 'Account Settings',
-                    size: 20.0,
-                    fWeight: FontWeight.w500,
-                  ),
-                ),
-                Container(
-                    height: accIcons.length * 55,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10)),
-                    child: ListView.builder(
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemCount: accIcons.length,
-                      itemBuilder: (context, index) => ListTile(
-                        leading: Icon(accIcons[index]),
-                        title: Text(accName[index]),
-                        trailing: const Icon(Icons.arrow_forward_ios_rounded),
-                      ),
-                    )),
               ],
             ),
-          ),
-        ));
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Center(
+                  child: MyText(
+                text: 'Accounts Centre',
+                fWeight: FontWeight.bold,
+                size: 18,
+              )),
+            ),
+            MyText(
+              text:
+                  'Manage your connected experiences and account settings across Meta technologies such as Facebook,Instagram and Meta Horizon',
+              fWeight: FontWeight.bold,
+              size: 18,
+              align: TextAlign.center,
+              color: Colors.grey[600],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    ListTile(
+                      leading: const CircleAvatar(
+                        backgroundImage: AssetImage(
+                            'assets/facebook/homepage/photo_2023-07-07_14-37-37.jpg'),
+                      ),
+                      title: const MyText(
+                        text: 'Profiles',
+                        fWeight: FontWeight.w500,
+                      ),
+                      subtitle: MyText(
+                        text: 'Hayat Khan',
+                        color: Colors.grey[600],
+                        size: 16,
+                      ),
+                      trailing: const Wrap(
+                        children: [
+                          MyText(text: '1'),
+                          Icon(Icons.arrow_forward_ios_rounded)
+                        ],
+                      ),
+                    ),
+                    const MyDivider(
+                      thick: 2,
+                    ),
+                    const ListTile(
+                      leading: Icon(
+                        Icons.connect_without_contact_sharp,
+                        size: 30,
+                      ),
+                      title: MyText(
+                        text: 'Connected experiences',
+                        fWeight: FontWeight.bold,
+                        size: 18,
+                      ),
+                      trailing: Icon(Icons.arrow_forward_ios_rounded),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 12.0),
+              child: MyText(
+                text: 'Account settings',
+                size: 17,
+                fWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  color: Colors.white,
+                  child: ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: accIcons.length,
+                    itemBuilder: (context, index) => Column(
+                      children: [
+                        ListTile(
+                          leading: Icon(
+                            accIcons[index],
+                            size: 26,
+                            color: Colors.black,
+                          ),
+                          title: MyText(
+                            text: accName[index],
+                            fWeight: FontWeight.bold,
+                            size: 16,
+                          ),
+                          trailing: const Icon(Icons.arrow_forward_ios_rounded),
+                        ),
+                        const MyDivider(
+                          thick: 1,
+                        )
+                      ],
+                    ),
+                  )),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
